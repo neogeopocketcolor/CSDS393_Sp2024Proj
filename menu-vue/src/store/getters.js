85fd69680ad0f2a6d8303cd8b4abcaf021bed1e8
k71707919 <k71707919@gmail.com>
2024-03-08 23:03:56 +0800

    1

--- menu-vue/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-  sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  userId: state => state.user.userId,
-  avatar: state => state.user.avatar,
-  role: state => state.user.role,
-  name: state => state.user.name
-}
-export default getters
